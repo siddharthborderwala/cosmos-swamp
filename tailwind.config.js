afc46a43ccdd9d7fc822ca605dcd9620d75110e2
Siddharth Borderwala <siddharthborderwala@gmail.com>
2024-09-03 19:28:34 +0530

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
+import { default as tailwindConfig } from '@leapwallet/react-ui/tailwind-config'
+
 /** @type {import('tailwindcss').Config} */
 export default {
+  ...tailwindConfig,
   content: [
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ]
